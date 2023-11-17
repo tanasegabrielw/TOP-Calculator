df628232a7d8a6a0ad432e964b56f8a6437b8689
Gabriel <tanase.gabriel671@gmail.com>
2023-11-17 12:01:11 +0200

    Created basic functions for each calculation

---
+++ code.js
@@ -0,0 +1,21 @@
+function add(numberOne, numberTwo){
+return numberOne+numberTwo;
+}
+
+function substract(numberOne, numberTwo){
+return numberOne - numberTwo;
+}
+
+function multiply(numberOne, numberTwo){
+return numberOne*numberTwo;
+}
+
+function divide(numberOne, numberTwo){
+return numberOne/numberTwo;
+
+}
+
+console.log(add(3,5));
+console.log(substract(3,10));
+console.log(multiply(3,43));
+console.log(divide(21,12));
